57e95c32897bfde05f8e41856bfbe6e291160b90
Eloi Alonso Quiñonero <97552454+Develoi89@users.noreply.github.com>
2023-09-28 16:06:46 +0200

    main file moded adding the test from subject

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,6 +4,22 @@
 int main()
 {
     {
+    int a = 2;
+    int b = 3;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+    {
     // float a = 12.3;
     // float b = 44.7;
     int a = 42;
